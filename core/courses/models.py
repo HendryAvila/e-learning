c67bd9a3621316c14d3f862630efe60f93a1c539
hendrycode <hduarteyt@gmail.com>
2025-08-30 20:15:48 -0300

    Creating an abstract model called ItemBase to inherance and create the different type of content

--- core/courses/models.py
+++ core/courses/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
 
 class Subject(models.Model):
@@ -58,7 +59,11 @@ class Content(models.Model):
     module = models.ForeignKey(
         Module, on_delete=models.CASCADE, related_name="contents"
     )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={"model__in": ("text", "video", "image", "file")},
+    )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
     order = models.PositiveIntegerField()
@@ -70,3 +75,34 @@ class Content(models.Model):
 
     def __str__(self):
         return f"Content for {self.module.title} - {self.item}"
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="%(class)s_related"
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to="files")
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to="images")
+
+
+class Video(ItemBase):
+    url = models.URLField()
